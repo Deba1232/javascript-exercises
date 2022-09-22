6fa054417f36bd15694e761ca0fbfe286dcddfd6
DeV <debanjan.das90@gmail.com>
2022-09-22 22:08:24 +0530

    solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let stringArray = [];
 
+    if (num < 0){
+        return 'ERROR';
+    }
+
+    else{
+        for(i=0;i<num;i++){
+            stringArray.push(str);
+        }
+        return stringArray.join("").toString();
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+   let charArray = str.split("");
+   let reverseCharArray = [];
 
+   for(i=charArray.length-1;i>=0;i--){
+    reverseCharArray.push(charArray[i]);
+   }
+
+   return reverseCharArray.join("").toString();
 };
 
 // Do not edit below this line
