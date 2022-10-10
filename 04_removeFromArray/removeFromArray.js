274866eaa23f742795fae1a738a46ea0753160ab
DeV <debanjan.das90@gmail.com>
2022-10-10 20:29:07 +0530

    edited js files

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...elementsToRemove) {
+    
+    for(i=0;i<elementsToRemove.length;i++){
 
+        if(arr.indexOf(elementsToRemove[i]) != -1){
+            let elementIndex = arr.indexOf(elementsToRemove[i]);
+            arr.splice(elementIndex,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum,secondNum) {
+    let sum = 0;
+
+    if ((firstNum > 0 && secondNum > 0) && (Number.isInteger(firstNum) && Number.isInteger(secondNum))){
+        if (firstNum < secondNum) {
+            for (i = 0; i < secondNum; i++) {
+                sum += firstNum;
+                firstNum++;
+            }
+        }
+
+        else if (firstNum > secondNum) {
+            for (i = 0; i < firstNum; i++) {
+                sum += secondNum;
+                secondNum++;
+            }
+        }
+    }
+
+    else{
+        return 'ERROR';
+    }
+    
+    return sum;
 
 };
 
