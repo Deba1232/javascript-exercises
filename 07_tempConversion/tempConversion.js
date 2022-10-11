49f6857f16392a7f430401b7163b4ba67a93f7be
DeV <debanjan.das90@gmail.com>
2022-10-11 19:23:51 +0530

    solutions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitTemp) {
+  let celTemp = (fahrenheitTemp - 32) * 5/9;
+  
+  if (Math.abs(celTemp % 1) > 0.01){
+    return parseFloat(celTemp.toFixed(1));
+  }
+  else{
+    return celTemp;
+  }
 
 };
 
-const ctof = function() {
-
+const ctof = function(celciusTemp) {
+  let fahrenTemp = ((9 * celciusTemp) / 5) + 32;
+  
+  if (Math.abs(fahrenTemp % 1) > 0.01) {
+    return parseFloat(fahrenTemp.toFixed(1));
+  }
+  else {
+    return fahrenTemp;
+  }
+  
 };
 
 // Do not edit below this line
